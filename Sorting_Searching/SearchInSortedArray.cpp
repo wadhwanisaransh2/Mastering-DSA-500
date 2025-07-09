c0f96c5ea9f273b4b255c4408ff0b5aa282c6a9c
wadhwanisaransh2 <wadhwanisaransh2@gmail.com>
2025-07-09 16:57:50 +0530

    Added binary search in rotated sorted array

---
+++ Sorting_Searching/SearchInSortedArray.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+
+            if (nums[mid] == target)
+                return mid;
+
+            // Left part is sorted
+            if (nums[mid] >= nums[low]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // Right part is sorted
+            else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1; // Target not found
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+
+    int result = sol.search(nums, target);
+    if (result != -1)
+        cout << "Target found at index: " << result << endl;
+    else
+        cout << "Target not found" << endl;
+
+    return 0;
+}
