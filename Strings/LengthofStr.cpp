54b585ab06453db5994f2bd9c0dd6804783c6232
wadhwanisaransh2 <wadhwanisaransh2@gmail.com>
2025-07-11 20:45:42 +0530

    Add LengthofStr.cpp to print string and its length using strlen

---
+++ Strings/LengthofStr.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    char str[] = "Saransh";
+    int len = 0;
+    for(int i=0 ; i<str[i]!='\0';i++) {
+        len++;
+    }
+    cout<<len;
+    return 0;
+}
