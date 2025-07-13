c41e0f9d53fa5ea8f646ff6500ec6586da479f5f
wadhwanisaransh2 <wadhwanisaransh2@gmail.com>
2025-07-13 07:32:10 +0530

    Added CreatingMatrix.cpp for 2D Arrays

---
+++ 2D_Arrays/CreatingMatrix.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int matrix[4][3];
+
+ for(int i=0;i<4;i++) {
+        for(int j=0;j<3;j++) {
+            cin>>matrix[i][j];
+        }
+        
+    }
+
+    for(int i=0;i<4;i++) {
+        for(int j=0;j<3;j++) {
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
