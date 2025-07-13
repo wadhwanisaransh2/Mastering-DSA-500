aacff883d5563791244bb104d78dfedc16c9898a
wadhwanisaransh2 <wadhwanisaransh2@gmail.com>
2025-07-13 23:26:13 +0530

    Added Maximum Sum of rows

---
+++ 2D_Arrays/MaxRowSum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int MaxrowSum(int matrix[][3],int r,int c){
+    int sum ;
+    int maxsum = INT8_MIN;
+    for(int i=0;i<r;i++) {
+        sum = 0;
+        for(int j=0;j<c;j++) {
+           sum += matrix[i][j];
+        }
+        maxsum = max(sum,maxsum);
+    }
+return maxsum;
+}
+int main() {
+    int matrix[3][3]={{1,2,3},{4,5,6},{7,-8,9}};
+    int key = 20;
+   int pos= MaxrowSum(matrix,3,3);
+   cout<<pos;
+    return 0;
+}
