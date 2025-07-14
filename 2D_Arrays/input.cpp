12da9fa246405ba8772615bb867d54ecd3e11a71
wadhwanisaransh2 <wadhwanisaransh2@gmail.com>
2025-07-14 23:29:38 +0530

    Added input.cpp

---
+++ 2D_Arrays/input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int matrix[4][3];
+
+ for(int i=0;i<4;i++) {
+        for(int j=0;j<3;j++) {
+            cin>>matrix[i][j];
+        }
+        
+    }
+
+    return 0;
+}
