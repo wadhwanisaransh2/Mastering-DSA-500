193316d54e39086e79e8e0d866a1999d1e53ea66
wadhwanisaransh2 <wadhwanisaransh2@gmail.com>
2025-07-20 22:47:06 +0530

    addedSortedArraycheck.cpp

---
+++ Recursion/SortedArraycheck.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+bool isSorted(int arr[], int n) {
+       if(n==1) {
+        return true;
+       }
+    if(arr[n-1]<arr[n-2]) {
+        return false;
+    }
+    return  isSorted(arr,n-1);
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 15, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    if (isSorted(arr, n))
+        cout << "Array is sorted\n";
+    else
+        cout << "Array is NOT sorted\n";
+
+    return 0;
+}
