2738637cdb1ad1d141d07e7f2d4848891f96439c
wadhwanisaransh2 <wadhwanisaransh2@gmail.com>
2025-07-20 22:45:34 +0530

    fibonacci.cpp

---
+++ Recursion/fibonacci.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    if(n == 0)
+        return 0;
+    if(n == 1)
+        return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+void printFibonacciSeries(int n) {
+    for(int i = 0; i < n; i++) {
+        cout << fibonacci(i) << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n = 8;
+    cout << "Fibonacci Series up to " << n << " terms:\n";
+    printFibonacciSeries(n);
+    return 0;
+}
