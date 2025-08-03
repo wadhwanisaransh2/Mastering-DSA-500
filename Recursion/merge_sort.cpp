e5ffc9870b43c198e49db9ce82e2830334c9cbcf
wadhwanisaransh2 <wadhwanisaransh2@gmail.com>
2025-08-03 23:30:25 +0530

    merge_sort.cpp

---
+++ Recursion/merge_sort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+// Function to merge two sorted subarrays
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1; // size of left subarray
+    int n2 = right - mid;    // size of right subarray
+
+    // Create temp arrays
+    int L[n1], R[n2];
+
+    // Copy data to temp arrays
+    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+
+    // Merge temp arrays back into arr
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    // Copy remaining elements
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
+// Recursive Merge Sort function
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
