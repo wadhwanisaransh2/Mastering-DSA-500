6023333aa2c30c33722702c53a14ef79e7d74811
wadhwanisaransh2 <wadhwanisaransh2@gmail.com>
2025-08-03 23:28:03 +0530

    added quicksort.cpp

---
+++ Recursion/quicksort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// Function to partition the array
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; // pivot element
+    int i = low - 1;       // index of smaller element
+
+    for (int j = low; j < high; j++) {
+        // If current element is smaller than pivot
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    // Place pivot in correct position
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+// Recursive Quick Sort function
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        // Partitioning index
+        int pi = partition(arr, low, high);
+
+        // Recursively sort elements before and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
