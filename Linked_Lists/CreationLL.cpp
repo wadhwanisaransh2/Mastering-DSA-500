1ac5ddb3088ae051dbd6a81abf0a10aa11822c4d
wadhwanisaransh2 <wadhwanisaransh2@gmail.com>
2025-08-09 10:50:29 +0530

    added push back function

--- Linked_Lists/CreationLL.cpp
+++ Linked_Lists/CreationLL.cpp
@@ -23,16 +23,23 @@ class List {
             return;
         }
         else{
-            newNode ->next = head;
+            newNode->next = head;
             head = newNode;
         }
+    }
+    void push_back(int val) {
+        Node *newNode = new Node(val);
+        tail->next = newNode;
+        tail = newNode;
     }
     void printLL(){
         Node *temp=head;
         while(temp!=NULL) {
-            cout<<temp->data;
+            cout<<temp->data<<"->";
             temp = temp->next;
         }
+
+        cout<<"NULL";
     }
 };
 int main() {
@@ -40,6 +47,7 @@ int main() {
     ll.push_front(1);
     ll.push_front(2);
     ll.push_front(3);
+    ll.push_back(4);
     ll.printLL();
     return 0;
 }
