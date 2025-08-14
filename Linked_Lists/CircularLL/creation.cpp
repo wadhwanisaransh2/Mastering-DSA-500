24ea91e4bfa1298372bddfe84b521db7b63f6598
wadhwanisaransh2 <wadhwanisaransh2@gmail.com>
2025-08-14 22:46:47 +0530

    added insert at begin function

--- Linked_Lists/CircularLL/creation.cpp
+++ Linked_Lists/CircularLL/creation.cpp
@@ -9,8 +9,44 @@ public:
         next= NULL;
     }
 };
+class CircularLL{
+    Node *head ;
+    Node *tail;
+public :
+    CircularLL() {
+        head = tail = NULL;
+    }
+    void InsertatHead(int val) {
+        Node *newnode = new Node(val);
+        if(head==NULL) {
+            head = tail = newnode;
+            tail -> next  = head;
+        }
+        else{
+            newnode->next = head;
+            head = newnode;
+            tail ->next = head;
+        }
+    }
 
+    void print() {
+        if(head == NULL) {
+            return;
+        }
+        cout<<head->data<<"<=>";
+        Node *temp = head->next;
+        while(temp!=head) {
+            cout<<temp->data<<"<=>";
+            temp = temp->next;
+        }
+        cout<<"NULL";
+    }
+};
 int main() {
-
+    CircularLL ll;
+    ll.InsertatHead(1);
+    ll.InsertatHead(2);
+    ll.InsertatHead(3);
+    ll.print();
     return 0;
 }
