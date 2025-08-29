a977ad348c092a12cd90dada53f82a494381126a
wadhwanisaransh2 <wadhwanisaransh2@gmail.com>
2025-08-29 23:22:42 +0530

    added merge interval.cpp

---
+++ Arrays/mergeintervaqls.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // Step 1: Sort intervals by start time
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> merged;
+        
+        for (auto interval : intervals) {
+            // If merged is empty OR no overlap
+            if (merged.empty() || merged.back()[1] < interval[0]) {
+                merged.push_back(interval);
+            }
+            else {
+                // Overlapping → merge by updating end
+                merged.back()[1] = max(merged.back()[1], interval[1]);
+            }
+        }
+        
+        return merged;
+    }
+};
