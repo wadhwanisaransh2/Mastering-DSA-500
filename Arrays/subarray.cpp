bae17cbd71cc01af21630b001fdb64631b8019fb
wadhwanisaransh2 <wadhwanisaransh2@gmail.com>
2025-08-31 23:41:27 +0530

    added 4 sum code

---
+++ Arrays/subarray.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> res;
+        int n = nums.size();
+        sort(nums.begin(), nums.end()); // Sort to handle duplicates easily
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue; // avoid duplicates
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j-1]) continue; // avoid duplicates
+
+                long long newTarget = (long long)target - nums[i] - nums[j];
+                int left = j + 1, right = n - 1;
+
+                while (left < right) {
+                    long long sum = nums[left] + nums[right];
+                    if (sum == newTarget) {
+                        res.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        left++; right--;
+
+                        while (left < right && nums[left] == nums[left-1]) left++;
+                        while (left < right && nums[right] == nums[right+1]) right--;
+                    }
+                    else if (sum < newTarget) left++;
+                    else right--;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {1,0,-1,0,-2,2};
+    int target = 0;
+
+    vector<vector<int>> ans = s.fourSum(nums, target);
+
+    cout << "Quadruplets: \n";
+    for (auto &quad : ans) {
+        for (int x : quad) cout << x << " ";
+        cout << "\n";
+    }
+    return 0;
+}
